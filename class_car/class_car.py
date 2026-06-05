30e0ece6b8668b62909c32da55d4bd7693d5119c
Dan <dllaneza552@gmail.com>
2026-06-05 23:45:04 +0800

    encapsulate year model and make attributes

--- class_car/class_car.py
+++ class_car/class_car.py
@@ -1,3 +1,5 @@
 class Car:
     def __init__(self, year_model, make):
-        pass 
+        # Initialize data attributes with double underscores for encapsulation
+        self.__year_model = year_model
+        self.__make = make
