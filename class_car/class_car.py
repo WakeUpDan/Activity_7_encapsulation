ccb7d7316e9dc3d449538c9b3d32bb1325725418
Dan <dllaneza552@gmail.com>
2026-06-05 23:47:12 +0800

    add get_speed accessor method

--- class_car/class_car.py
+++ class_car/class_car.py
@@ -4,3 +4,6 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
+    def get_speed(self):
+        # Return the current speed
+        return self.__speed
