1e572152cb204b42ee115137e9dff3bfefdafd53
Dan <dllaneza552@gmail.com>
2026-06-05 23:59:28 +0800

    add mutator method for pet name

--- pet_class/pet_class.py
+++ pet_class/pet_class.py
@@ -4,3 +4,8 @@ class Pet:
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
+    # --- Mutator (Setter) Methods ---
+    def set_name(self, name):
+        # Assigns a value to the __name field
+        self.__name = name 
+
