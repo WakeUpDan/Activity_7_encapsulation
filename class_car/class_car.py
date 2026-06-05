9563479001dce4c08f5501027e387c5db7f12c66
Dan <dllaneza552@gmail.com>
2026-06-05 23:45:54 +0800

    initialize default private speed state to 0

--- class_car/class_car.py
+++ class_car/class_car.py
@@ -3,3 +3,4 @@ class Car:
         # Initialize data attributes with double underscores for encapsulation
         self.__year_model = year_model
         self.__make = make
+        self.__speed = 0
